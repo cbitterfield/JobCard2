43c2a76dc0e15e29ce5c4342bf0ec55f7c602e9e
cbitterfield <colin@bitterfield.com>
2017-12-27 13:21:25 -0500

    Added a task to copy and convert files as needed.

--- src/task.py
+++ src/task.py
@@ -106,70 +106,48 @@ def numimages(source, config, noexec):
             
     return(Error, jpg, tif)
 
-def filetransfer(config, location, account, filename, path, noexec):
+def filetransfer(username, password, host, filename, destination):
     logger.info("Starting filetransfer")
     Error = False
     
-    # Assign Variables from Config
-    try:
-        ftpsite = config[location]['ftpsite'] if "ftpsite" in config[location] else None
-        password = config[location][account] if account in config[location] else None
-        basename = os.path.basename(filename)
-        logger.info("FTP Site: " + str(ftpsite))
-        logger.info("Account: " + str(account))
-        logger.info("Password: " + str(password))
+    # Get basename of the file
+    basename = os.path.basename(filename)
+    
+    #Verify file exists.
+    if os.path.isfile(filename): 
+        logger.info( str(filename) + " file exists" )
         filehandle = open(filename,'rb')
-        if os.path.isfile(filename):
-            logger.info("File " + str(basename) + " exists")
-        else:
-            Error = True
-            logger.error("File " + str(basename) + " does not exists")
-        
-        # Open FTP Connection
-        
-        ftp = ftplib.FTP(ftpsite,account,password)
-        logger.info(ftp.getwelcome())
-        
-        # Set Passive Mode
+    else:
+        Error = True
+        logger.error( str(filename) + " file does not exists" )
+        return Error
+    
+
+    # Connect with FTP Host
+    logger.info("Connecting to " + str(host) + " as USER " + str(username))
+    logger.info("Sending file: " + str(basename) + " to " + str(destination))
+    logger.debug("using password " + str(password))
+    try:
+        ftp = ftplib.FTP(host,username,password)
+        logger.debug( ftp.getwelcome() )
+
         ftp.set_pasv(True)   
-        
-        # Change to and check destination
+        ftp.cwd(destination)
+        logger.debug(ftp.pwd())
+            
         try:
-            logger.info(ftp.cwd(path))
-        except Exception as e:
-            logger.error(ftplib.error_perm)
-            logger.error("Can't change directory")
-            logger.warn("An error occured " + str(e)) 
-            Error = True
+            ftp.size(basename)
+            logger.warn( "File Exists: " + str(basename))
+        except:
+            logger.info("Uploadling new file " + str(basename))
                 
-        logger.info("FTP Remote Directory: " + str(ftp.pwd()))
-        
-        if Error == False:
-        
-            try:
-                ftp.size(basename)
-                logger.info( str(basename) +  " File Exists, existing file will be overwritten")
-            except:
-                logger.info("Uploadling new file:" + str(basename))
-            
-            try:            
-                ftp_message = ftp.storbinary('STOR ' + str(basename), filehandle, 8192)
-                logger.info(ftp_message)
-            except Exception as e:
-                logger.warn("An error occured " + str(e)) 
-                Error = True
-        else:
-            logger.error("Processing terminated, an error occurred")
-            
-        logger.info("Close FTP Connection")
-        ftp.close()
-        
+        logger.info( ftp.storbinary('STOR ' + str(basename), filehandle, 8192) )
+                
     except Exception as e: 
         Error = True
-        logger.warn("An error occured " + str(e))    
-    
-    
-    
+        logger.error("An error occured " + str(e))
+        
+    ftp.close()
     return Error
  
 def test(test_text):
@@ -178,12 +156,14 @@ def test(test_text):
     
     return(test)   
 
-def getmd5(config, filename, noexec):
-    MD5=config['locations']['md5']
-    value=subprocess.check_output([MD5, filename])
-    myMD5,basename = value.split()
-    logger.info("MD5: " + filename + " = " + myMD5)
-    return myMD5
+def md5(fname):
+    import hashlib
+    logger.info("Generating md5 for " + str(fname))
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
 
 def makeList (jobcard,object_eval):
@@ -267,3 +247,161 @@ def validateitem(config, jobcard, volume, test_component):
             
     return Error
 
+def copyconvert(source, destination, edgeid, ext, suffix, recursive, config, noexec):
+    import os
+    import subprocess
+    import shutil
+
+    Error = False
+    
+    # Setup program variables
+    IDENTIFY = config['programs']['identify']
+    CONVERT = config['programs']['convert']
+    
+    FileList = []
+    
+    #===========================================================================
+    # Debug Values
+    #===========================================================================
+    logger.debug("Identify located " + str(IDENTIFY))
+    logger.debug("Convert located " + str(CONVERT))
+    logger.debug("Source: " + str(source))
+    logger.debug("Destination: " + str(destination))
+    logger.debug("Suffix: " + str(suffix))
+    logger.debug("Extenstion: " + str(ext))
+    logger.debug("Recursive: " + str(recursive))
+    logger.debug("Noexec: " + str(noexec))
+
+
+    #===========================================================================
+    # Make a list of files to work from
+    # Validate Source even exists
+    #===========================================================================
+    
+    if os.path.exists(source):
+        pathName = os.path.dirname(source)
+        if os.path.isfile(source):
+            IsFile = True
+            
+        else:
+            IsFile = False
+                
+                
+            
+    else:
+        Error = True
+        logger.error("Source " + str(source) + " does not exist")
+        return Error
+    
+    if IsFile:
+        FileList.append(source)
+            
+            
+    else:
+        for eachfile in os.listdir(source):
+            if (not eachfile.startswith(".")) and (os.path.isfile(str(source) + "/" + str(eachfile))):
+                FileList.append(str(source) + "/" + str(eachfile))
+            else:
+                logger.debug("ignoring " + str(eachfile))
+
+    logger.debug(FileList)
+    logger.debug(pathName)
+    
+    #===========================================================================
+    # Make Directory if needed
+    #===========================================================================
+    
+    if not os.path.exists(destination) and not noexec:
+        logger.debug("Creating Directory for Destination " + str(destination))
+        os.makedirs(destination)
+    else:
+        logger.debug("Destination directory " + str(destination))
+    
+    #===========================================================================
+    # What type of file are we dealing with
+    # Deal with Image, Video, PDF or other types
+    #===========================================================================
+    
+    
+    #===========================================================================
+    # Dealing with files that ImageMagick can deal with.
+    #===========================================================================
+    
+    if (ext == '.jpg' ) or  (ext == '.jpeg' ) or (ext == '.tif' ) or (ext == '.tiff' ) or (ext == '.gif' ) or (ext == '.png' ) or (ext == '.pdf' ):
+        logger.debug("image files")
+        # Image files will convert to ext format. This includes PDFs
+        for copyFile in FileList:
+    
+                
+            if copyFile.endswith(".jpg") or copyFile.endswith(".jpeg") or copyFile.endswith(".tiff") or copyFile.endswith(".tiff") or copyFile.endswith(".png") or copyFile.endswith(".gif") or copyFile.endswith(".pdf"):
+                baseName = os.path.basename(copyFile)
+                Name, extName = baseName.split(".")
+                # Check to see if destination file exsits.        
+                newName = str(edgeid) + str(suffix) + str(ext)
+                proposeName = str(destination) +"/" + str(newName)
+                    
+                if os.path.isfile(proposeName):            
+                    newName = str(edgeid) + str(suffix) + "[" + str(baseName) + "]" + str(ext) 
+                    logger.warn("Destination File exists, new name " + newName)
+                else:
+                    logger.info("New Name is " + str(newName))
+                
+                if ext == str('.' + extName):
+                    logger.debug("simple copy for " + str(baseName))
+                else:
+                    # Convert file from existing format to requested format
+                    logger.debug("Convert File")
+                    CMD = CONVERT + " '" + str(copyFile) + "' '" + destination + "/" +str(newName) + "'"
+                    logger.debug("Command: " + str(CMD)) 
+                    if not noexec:          
+                        CMD_result = subprocess.Popen(CMD, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)     
+                        stdoutdata, stderrdata = CMD_result.communicate()
+                        CMD_status = CMD_result.returncode
+                    
+        
+            else:
+                logger.warn("Improper file type detected, simple copy will occur")
+                baseName = os.path.basename(copyFile)
+                Name, extName = baseName.split(".")
+                origExt = "." + extName
+                # Check to see if destination file exsits.        
+                newName = str(edgeid) + str(suffix) + str(origExt)
+                proposeName = str(destination) +"/" + str(newName)
+                
+                if os.path.isfile(proposeName):            
+                    newName = str(edgeid) + str(suffix) + "[" + str(baseName) + "]" + str(origExt) 
+                    logger.warn("Destination File exists, new name " + newName)
+                else:
+                    logger.info("New Name is " + str(newName))
+                try:
+                    logger.debug("Copy " + copyFile  + " => " + destination + "/" + newName)
+                    shutil.copy(copyFile, destination + "/" + newName)  if not noexec else logger.info("Noexec Flag")
+                except Exception as e: 
+                    logger.error(e)
+                    Error = True
+                
+    else:
+        # Everything else will be a simple copy.
+        logger.debug("Non-Image files")
+        for copyFile in FileList:
+            baseName = os.path.basename(copyFile)
+            Name, extName = baseName.split(".")
+            origExt = "." + extName
+            # Check to see if destination file exsits.        
+            newName = str(edgeid) + str(suffix) +  str(origExt)
+            proposeName = str(destination) +"/" + str(newName)
+                    
+            if os.path.isfile(proposeName):            
+                newName = str(edgeid) + str(suffix) + "[" + str(baseName) + "]" + str(origExt) 
+                logger.warn("Destination File exists, new name " + newName)
+            else:
+                logger.info("New Name is " + str(newName))
+            try:
+                logger.debug("Copy " + copyFile  + " => " + destination + "/" + newName)
+                shutil.copy(copyFile, destination + "/" + newName)  if not noexec else logger.info("Noexec Flag")
+            except Exception as e: 
+                logger.error(e)
+                Error = True    
+            
+    
+    return Error
